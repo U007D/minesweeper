e757b5fe5bbe71b81f9255e3bb5d82ee1072d997
Brad Gibson <b2b@humanenginuity.com>
2019-01-24 11:31:04 -0800

    new_with_double_digit_r_and_c_yields_view_which_display_only_hidden_cells: green

--- src/view.rs
+++ src/view.rs
@@ -33,7 +33,7 @@ impl<T: BufRead> View<T> {
         // rows * cols should fit into a usize on a 32-bit machine (ie. a u32)
         debug_assert!(model.rows() <= u16::MAX as usize);
         debug_assert!(model.cols() <= u16::MAX as usize);
-        // cannot truncate ∵ both `model.rows()` and `model.columns()` values are <= 16 bits
+        // cannot truncate ∵ both `model.rows()` and `model.cols()` values are limited to 16 bits
         // cannot lose sign ∵ intermediate `f64` value is always positive
         #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
             Self {
@@ -45,29 +45,28 @@ impl<T: BufRead> View<T> {
     }
 
     fn col_headers(&self) -> String {
-        const BASE_10: u32 = 10;
-        // cannot overflow ∵ `model.columns()` is bound to 16 bits which limits `max_row_digits` to 5 (3 bits).
+        const BASE_10: usize = 10;
+        // cannot overflow ∵ `model.cols()` is bound to 16 bits which limits `max_row_digits` to 5 (3 bits).
         // TODO: fix possible overflow due to headers
         #[allow(clippy::integer_arithmetic)]
             let mut result = String::with_capacity((self.max_row_digits + 1 + self.model.cols()) * (self.max_col_digits + 1));
-        (1..=self.max_col_digits).rev()
-                                 .for_each(|p| {
-                                     // cannot truncate ∵ `p` cannot exceed 5 (# of digits in base 10 u16::MAX).
-                                     #[allow(clippy::cast_possible_truncation)]
-                                         let pow_10 = 10_usize.pow(p as u32);
+        (0..self.max_col_digits).rev()
+                                .for_each(|exp| {
                                      result += &(" ".repeat(self.max_row_digits) + "|");
-                                     // cannot overflow ∵ `model.colums()` is bound to 16 bits, which, in turn limits
-                                     // `max_row_digits` to 5 (3 bits).
-                                     // *1 cannot truncate ∵ `d % 10` never exceeds 9.
-                                         #[allow(clippy::integer_arithmetic, clippy::cast_possible_truncation)]
-                                     (1..(self.max_row_digits + self.model.cols())).for_each(|c| {
-                                         result += &match c % pow_10 {
+                                    // cannot truncate ∵ `exp` cannot exceed 5 (# of base 10 digits in u16::MAX).
+                                    #[allow(clippy::cast_possible_truncation)]
+                                        let pow = BASE_10.pow(exp as u32);
+                                    // cannot overflow ∵ `model.cols()` is limited to 16 bits, which, in turn limits
+                                    // `max_row_digits` to 5 base 10 digits (== 3 bits).
+                                    // *1 cannot truncate ∵ `x % 10` never exceeds 9.
+                                        #[allow(clippy::integer_arithmetic, clippy::cast_possible_truncation)]
+                                    (1..=self.model.cols()).for_each(|c| {
+                                        result += &match c < pow {
+                                            true => ' ',
                                              // *1
-                                             d if d > 0 => char::from_digit((d % 10) as u32, BASE_10)
-                                                 .expect(msg::ERR_INTERNAL_DIGIT_NOT_BASE_10)
-                                                 .to_string(),
-                                             _ => ' '.to_string(),
-                                         };
+                                            false => char::from_digit((c / pow % BASE_10) as u32, BASE_10 as u32)
+                                                .expect(msg::ERR_INTERNAL_DIGIT_NOT_BASE_10),
+                                        }.to_string();
                                      });
                                      result += "\n";
                                  });
@@ -76,7 +75,7 @@ impl<T: BufRead> View<T> {
     }
 
     fn row_header(&self, row: usize) -> String {
-        format!("{1:0$}|", self.max_row_digits, row.to_string())
+        format!("{1:>0$}|", self.max_row_digits, row.to_string())
     }
 }
 
--- src/view/unit_tests.rs
+++ src/view/unit_tests.rs
@@ -38,3 +38,32 @@ fn new_yields_view_which_display_only_hidden_cells() {
 // then the resulting instance should contain the expected model
     assert_eq!(format!("{}", result), expected_string);
 }
+
+#[test]
+fn new_with_double_digit_r_and_c_yields_view_which_display_only_hidden_cells() {
+// setup
+    let rows = BoardDimension::new(11).unwrap();
+    let cols = BoardDimension::new(12).unwrap();
+    let prob = Probability::new(0).unwrap();
+    let model = GameBoard::new(rows, cols, prob);
+    let reader = StdInMock::new(Vec::<&str>::new());
+    let mut expected_cells = Vec::with_capacity(model.rows() * model.cols());
+    model.iter()
+         .flat_map(|vec| vec.iter())
+         .for_each(|&v| expected_cells.push(v));
+    let mut expected_string = String::from("  |         111\n  |123456789012\n--+------------\n");
+    (1..=model.rows()).for_each(|r| {
+        expected_string += &format!("{1:>0$}|", 2, r);
+        (1..=model.cols()).for_each(|_c| expected_string += " ");
+        expected_string += "\n";
+    });
+
+// given a constructor
+    let sut = View::new;
+
+// when it is invoked
+    let result = sut(model, reader);
+
+// then the resulting instance should contain the expected model
+    assert_eq!(format!("{}", result), expected_string);
+}
